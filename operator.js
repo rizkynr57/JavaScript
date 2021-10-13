8953facb29c136dde76e5de6f893464c8c3ba634
Rizky Nurahman <90430417+Rizkynr90@users.noreply.github.com>
2021-10-13 08:27:14 -0700

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,11 @@
+var x = 10;
+x += 25; // Hasilnya 35
+
+var txt1 = "Saya Suka";
+var txt2 = "Nasi";
+var txt3 = "Goreng";
+var txt4 = txt1 + " " + txt2 + " " + txt3 // Hasilnya 'Saya Suka Nasi Goreng'
+
+var x = 5 + 5; // Hasilnya 10
+var y = "5" + 5; // Hasilnya 55
+var z = "Balon Saya" + 5; // Hasilnya Balon Saya5
